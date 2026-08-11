378b2df48c0c5f34b9df757c7381db7b26603c2a
Madhuri reddy <madhurireddy185@gmail.com>
2026-08-11 22:24:29 +0530

    Add files via upload
    bitwise

---
+++ bitwise.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class bitwise {
+    public static void main(String[] args) {
+        char beta=103;int delta='A';//type casting
+        System.out.println(beta^delta);
+        System.out.println(beta&'d');
+        System.out.println(delta|592);
+        System.out.println(beta<<4);
+        System.out.println(delta>>8);
+    }
+}
